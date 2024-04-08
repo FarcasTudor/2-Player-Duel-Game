75468f649a065fb5b91e46e5c220b8ac668ec165
Tudor Farcas <tudor.farcas2@stud.ubbcluj.ro>
2024-04-08 22:20:23 +0300

    Added scoring between scenes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -17,24 +19,50 @@ public class GameManager : MonoBehaviour
     public GameObject[] p1Sticks;
     public GameObject[] p2Sticks;
 
+    public string mainMenu;
+
+    public Text scoreText;
+    private static int P1Score = 0;
+    private static int P2Score = 0;
+    private static bool P1HasWon = false;
+    private static bool P2HasWon = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateScoreText(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(P1Life <= 0)
+        if (P1Life <= 0 && !P2HasWon)
         {
             player1.SetActive(false);
             p2Wins.SetActive(true);
+            P2Score += 1;
+            UpdateScoreText();
+            P2HasWon = true; 
         }
-        if (P2Life <= 0)
+        if (P2Life <= 0 && !P1HasWon)
         {
             player2.SetActive(false);
             p1Wins.SetActive(true);
+            P1Score += 1;
+            UpdateScoreText();
+            P1HasWon = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            P1HasWon = false ; P2HasWon = false ;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(mainMenu);
+            // poate trb adaugat , vad daca ma decid 
         }
     }
 
@@ -69,5 +97,10 @@ public class GameManager : MonoBehaviour
             }
         }
         hurtSound.Play();
+    }
+
+    void UpdateScoreText()
+    {
+        scoreText.text = $"{P1Score} - {P2Score}";
     }
 }
